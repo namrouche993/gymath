2903c0c3e41cac2ee8c5e13383a3cb4be1fb4f32
namrouche993 <namrouche993@gmail.com>
2022-09-28 00:38:25 +0100

    until flexdirection

--- src/Contact.js
+++ src/Contact.js
@@ -1,17 +1,42 @@
-import { Grid,Box } from '@mui/material'
+import { Grid,Box, Paper } from '@mui/material'
 import React from 'react'
 
 export default function Contact() {
   return (
     <div>
+    <Grid container direction='column'>
       <Grid container direction='row' sx={{display:'flex'}}>
-          <Grid xs={12} md={6} sx={{lineHeight:5,textAlign:'center' ,fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
+          <Grid xs={12} md={3} sx={{lineHeight:5,textAlign:'center' ,fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
               0
           </Grid>
-          <Grid xs={12} md={6} sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
+          <Grid xs={12} md={3} sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
                0.1
           </Grid>
-      </Grid>
+          <Grid xs={6} md={3} sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
+              2
+          </Grid>          
+          <Grid xs={6} md={3} sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
+              2
+          </Grid>
+       </Grid>
+       <Grid container sx={{display:'flex',flexDirection:'column'}}>
+          <Grid item xs={3} md={2} sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
+              2.1
+          </Grid> 
+          <Grid item xs={3} md={2} sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
+              2.2
+          </Grid>           
+          <Grid item xs={4} md={2} sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
+              2.3
+          </Grid> 
+          <Grid item xs={4} md={2} sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
+              2.4
+          </Grid>
+       </Grid>
+
+
+
+    </Grid>
     </div>
   )
 }
