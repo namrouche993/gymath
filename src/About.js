3a0af054ad4be250a1ad03a3ebd56ce9bbdbb9f9
Mohamed Nadjib AMROUCHE <m.amrouche@habitat.mh>
2022-10-04 15:40:11 +0100

    triying to fix props between 2component

--- src/About.js
+++ src/About.js
@@ -1,65 +1,100 @@
-import React from 'react'
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Typography from '@mui/material/Typography';
+//https://mui.com/material-ui/react-dialog/
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
 
-export default function About() {
+const BootstrapDialogTitle = (props) => {
+  const { children, onClose, ...other } = props;
+
   return (
-    <div>About
-      <h3>
- If you want to start measuring performance in your app, pass a function
- to log results 
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+};
 
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals <br></br>
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-If you want to start measuring performance in your app, pass a function
- to log results 
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals <br></br>
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-If you want to start measuring performance in your app, pass a function
- to log results 
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals <br></br>
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-If you want to start measuring performance in your app, pass a function
- to log results 
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals <br></br>
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-If you want to start measuring performance in your app, pass a function
- to log results 
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals <br></br>
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-If you want to start measuring performance in your app, pass a function
- to log results 
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+BootstrapDialogTitle.propTypes = {
+  children: PropTypes.node,
+  onClose: PropTypes.func.isRequired,
+};
 
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals <br></br>
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-If you want to start measuring performance in your app, pass a function
- to log results 
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals <br></br>
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-If you want to start measuring performance in your app, pass a function
- to log results 
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals <br></br>
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-If you want to start measuring performance in your app, pass a function
- to log results 
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals <br></br>
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-If you want to start measuring performance in your app, pass a function
- to log results 
-or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-      </h3>
+export default function About(props) {
+  
+   const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+
+   const handleClose = () => {
+      setOpen(true);
+    };
+
+  return (
+     <div>
+       <Button variant="outlined" onClick={handleClickOpen}>
+         Open dialog
+       </Button>
+      <BootstrapDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
+          Modal title
+        </BootstrapDialogTitle>
+        <DialogContent dividers>
+          <Typography gutterBottom>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+            consectetur ac, vestibulum at eros.
+          </Typography>
+          <Typography gutterBottom>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          </Typography>
+          <Typography gutterBottom>
+            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
+            magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
+            ullamcorper nulla non metus auctor fringilla.
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose}>
+            Save changes
+          </Button>
+        </DialogActions>
+      </BootstrapDialog>
     </div>
-  )
+  );
 }
--- src/Contact.js
+++ src/Contact.js
@@ -11,35 +11,46 @@ export default function Contact() {
 
   return (
     <div>
-    <Grid container direction='column'>
-      <Grid container direction='row' sx={{display:'flex'}}>
+    <Grid container sx={{flexDirection:'column',display:'flex'}}>
+       {/* direction='column'> */}
+      <Grid container item sx={{display:'flex',flexDirection:'row'}}> 
+       {/* direction='row' }} > */}
           <Grid xs={12} md={3} sx={{lineHeight:5,textAlign:'center' ,fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
              1
           </Grid>
           <Grid xs={12} md={3} sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
              2
           </Grid>
-          <Grid xs={6} md={3} sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
+          <Grid xs={6} md={4} sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
              3
           </Grid>          
-          <Grid xs={6} md={3} sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
+          <Grid xs={6} md={2} sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
              4
           </Grid>
-       </Grid>
-       <Grid container sx={{display:'flex',color:{xs:'red',md:'blue'}, flexDirection: {xs:'row' , md:'column'}  }}>
-          <Grid item xs={3} md={2} sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
+       </Grid>  
+    <Grid container item md={6} sx={{display:'flex',flexDirection:'row'}}>    
+      <Grid container item md={4} sx={{display:'flex',color:{xs:'red',md:'blue'}, flexDirection: {xs:'row' , md:'column'}  }}>
+        {/* <Grid sx={{flexDirection:{md:'column'}}}> */}
+          <Grid item xs={3} md={12} sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
             5
           </Grid> 
-          <Grid item xs={3} md={2} sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
+          <Grid item xs={3} md={12} sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
             6
           </Grid>           
-          <Grid item xs={3} md={2} sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
+          <Grid item xs={3} md={12} sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
             7
           </Grid> 
-          <Grid item xs={3} md={2} sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
+          <Grid item xs={3} md={12} sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
             8
           </Grid>
-       </Grid>
+        {/* </Grid> */}
+      </Grid>
+      <Grid container item md={6}>
+        <Grid item sx={{lineHeight:4,textAlign:'center',fontSize:'1.5rem',boxShadow:'5px 5px 8px 2px rgb(20 25 30 / 30%)'}}>
+           9
+        </Grid>
+      </Grid>
+    </Grid>
 
 
 
