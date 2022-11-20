831111a364c0e9c10f83681107f5b765b9291712
Mohamed Nadjib AMROUCHE <m.amrouche@habitat.mh>
2022-11-20 09:53:19 +0100

    change repository name

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ import Button_add_dialog from './component/Button_add_dialog.js';
 export default function Home() {
   return (
     <div> 
-      {/* <Button_add_dialog/> */}
+      {/* <Button_add_dialog/> !! */}
       <Tableabonnement/>
     </div>
   )
